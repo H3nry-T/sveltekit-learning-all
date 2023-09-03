24af5f508eb972cbe3ff1a8ae5508bd94f40caca
H3nry-T <ht3drex@gmail.com>
2023-09-03 15:38:47 +0100

    removed the kanban file for svelte-kanban

--- kanban/src/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_PROJECT_ANON_KEY } from '$env/static/public';
-import { SECRET_PROJECT_URL } from '$env/static/public';
-
-const supabaseUrl = SECRET_PROJECT_URL;
-const supabaseAnonKey = PUBLIC_PROJECT_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
