3c21de83e4ee6a7e43e0064e2037258867d3c95b
H3nry-T <ht3drex@gmail.com>
2023-09-08 11:31:16 +0100

    adding card plays green border animation

---
+++ svelte-kanban/src/lib/components/KanbanPage/KanbanCard.svelte
@@ -0,0 +1,102 @@
+<script>
+	import { Badge } from '$lib/components/ui/badge/index';
+	import { Button } from '$lib/components/ui/button/index';
+	import * as Card from '$lib/components/ui/card/index';
+	import { Checkbox } from '$lib/components/ui/checkbox/index';
+	import { deleteTodos, updateToggleTodos } from '$lib/stores/todosStore';
+	import { X } from 'lucide-svelte';
+
+	/**
+	 * @type {import('$lib/stores/todosStore').Todo} todo
+	 */
+	export let todo;
+
+	/**
+	 * @type {boolean} animateAddCard
+	 */
+	export let animateAddCard = false;
+
+	/**
+	 * @type {boolean} animateEdit
+	 */
+	let animateEdit = false;
+	function playEditAnimation() {
+		animateEdit = true;
+		const clearTimeout = setTimeout(() => {
+			animateEdit = false;
+		}, 500);
+	}
+</script>
+
+<Card.Root
+	class="min-w-fit  transition-all duration-300 ease-in-out relative  
+	{animateEdit ? 'border border-accent-foreground' : null}
+	{animateAddCard ? 'border border-green-700 -translate-y-0 opacity-100' : null}
+	"
+>
+	<Button
+		variant="destructive"
+		class="absolute top-1 right-1 h-[25px] p-0 bg-clip-text"
+		size="default"
+		on:click={() => deleteTodos(todo.id)}
+		><X class="text-primary" />
+	</Button>
+
+	<section class="flex">
+		<Card.Header class="py-2 pl-3 max-w-[220px] 2xl:max-w-xs">
+			<Card.Title
+				class="text-base md:text-lg transition-colors duration-1000  ease-linear truncate first-letter:capitalize {todo.is_done ||
+				todo.column_number === 3
+					? 'line-through text-muted-foreground'
+					: ''}">{todo.title}</Card.Title
+			>
+			<!-- {#if todo.description.length > 0}
+				<Card.Description class="truncate">
+					description here Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis sed
+					nostrum non laborum esse voluptates, doloribus exercitationem dolor, ad atque id quas
+					velit inventore adipisci quod unde, pariatur quisquam dolorum!</Card.Description
+				>
+			{/if} -->
+		</Card.Header>
+		<section class="flex justify-between w-full">
+			<Checkbox
+				checked={todo.is_done || todo.column_number === 3}
+				onCheckedChange={() => {
+					updateToggleTodos(todo.id, todo.is_done);
+					playEditAnimation();
+				}}
+				class="self-center"
+			/>
+		</section>
+	</section>
+	<Card.Footer class="flex items-center px-2 py-2">
+		<div class="">
+			{#if todo.column_number === 1}
+				<Badge class="text-blue-800 bg-blue-300 pointer-events-none " variant="outline"
+					>ice-box</Badge
+				>
+			{:else if todo.column_number === 2}
+				<Badge class="text-yellow-800 bg-yellow-300 pointer-events-none" variant="secondary"
+					>in-progress</Badge
+				>
+			{:else}
+				<Badge class="text-green-800 bg-green-300 pointer-events-none">done</Badge>
+			{/if}
+		</div>
+	</Card.Footer>
+</Card.Root>
+
+<style>
+	.animate-slide-in {
+		animation: slideIn 500ms ease-in;
+	}
+
+	@keyframes slideIn {
+		0% {
+			transform: translateY(100px);
+		}
+		100% {
+			transform: translateY(0);
+		}
+	}
+</style>
--- svelte-kanban/src/routes/+page.svelte
+++ svelte-kanban/src/routes/+page.svelte
@@ -1,63 +1,109 @@
 <script>
+	import KanbanCard from '$lib/components/KanbanPage/KanbanCard.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import { userStore } from '$lib/stores/authStore';
-	import { addTodos, deleteTodos, todos, updateToggleTodos } from '$lib/stores/todosStore';
+	import { addTodos, todos } from '$lib/stores/todosStore';
+	import { Plus } from 'lucide-svelte';
+	import { fade } from 'svelte/transition';
 
 	let form = {
 		title: ''
 	};
+	/**
+	 * @type {boolean} animateAddCard;
+	 */
+	let animateAddCard = false;
+
+	/**
+	 * starts the animation for when adding a new todo card
+	 * @param {number} duration - milliseconds
+	 */
+	function playAddCardAnimation(duration) {
+		animateAddCard = true;
+		const clearTimeout = setTimeout(() => {
+			animateAddCard = false;
+		}, duration);
+	}
 </script>
 
-<section class="min-w-full min-h-screen px-20 py-10">
-	<h1 class="text-2xl font-semibold leading-none capitalize md:text-4xl">svelte-kanban</h1>
-	<section class="flex flex-col">
-		<h2>kanban section goes here</h2>
-		<form
-			class="flex items-center justify-center w-full max-w-2xl gap-4 p-4 mx-auto text-gray-100 rounded-lg bg-card text-card-foreground"
-			on:submit|preventDefault
-		>
-			<fieldset class="flex items-center w-full gap-4">
-				<Input
-					class="w-full max-w-2xl py-0"
-					bind:value={form.title}
-					type="text"
-					name="title"
-					id="title"
-					required
-					placeholder="Add todo here"
-				/>
-				<Button
-					class="py-0"
-					on:click={async () => {
-						if (form.title.length > 0) {
-							await addTodos(form, $userStore?.id);
+<section class="min-w-full min-h-screen px-5 md:px-20">
+	<h1 class="mb-10 text-2xl font-semibold leading-7 capitalize md:text-4xl">svelte-kanban</h1>
 
-							form.title = '';
-						}
-					}}>submit</Button
+	<form
+		class="flex items-center justify-center w-full max-w-sm gap-4 mb-5 rounded-lg bg-card text-card-foreground"
+		on:submit|preventDefault={async () => {
+			if (form.title.length > 0) {
+				await addTodos(form, $userStore?.id);
+				playAddCardAnimation(1000);
+				form.title = '';
+			}
+		}}
+	>
+		<fieldset class="flex items-center w-full gap-4">
+			<Input
+				class="w-full max-w-2xl py-0"
+				bind:value={form.title}
+				type="text"
+				name="title"
+				id="title"
+				required
+				placeholder="Add todo"
+			/>
+			<Button
+				on:click={() => {
+					if (form.title.length > 0) playAddCardAnimation(1000);
+				}}
+				class="py-0"
+				size={'icon'}
+				type="submit"><Plus /></Button
+			>
+		</fieldset>
+	</form>
+
+	<section class="pb-10 overflow-x-scroll lg:overflow-x-auto">
+		<section class="grid items-start grid-cols-3 gap-4 mt-10 min-w-[1000px]">
+			<Card.Root class="flex flex-col w-full gap-4 p-5 pt-4 min-w-fit">
+				<h2
+					class="text-xl font-semibold tracking-tight md:text-2xl first-letter:capitalize text-secondary-foreground"
 				>
-			</fieldset>
-		</form>
-	</section>
-	<section class="flex flex-col max-w-2xl gap-4 p-8 mx-auto mt-10 bg-gray-300 rounded-lg">
-		{#each $todos as todo (todo.id)}
-			<div class="flex items-center gap-4 p-4 bg-gray-400 rounded-lg">
-				<input
-					class="w-5 h-5 rounded-md form-checkbox"
-					type="checkbox"
-					name="is_done"
-					id="is_done"
-					value={todo.is_done}
-					checked={todo.is_done}
-					on:change={() => updateToggleTodos(todo.id, todo.is_done)}
-				/>
-				<p class={todo.is_done ? 'line-through' : null}>{todo.title}</p>
-				<button
-					class="px-1 py-2 ml-auto leading-none text-red-900 bg-red-300 rounded-lg"
-					on:click={() => deleteTodos(todo.id)}>🗑️</button
+					ice-box
+				</h2>
+				{#each $todos as todo (todo.id)}
+					{#if todo.column_number === 1}
+						{#if todo.id === $todos[$todos.length - 1].id && animateAddCard}
+							<KanbanCard {todo} {animateAddCard} />
+						{:else}
+							<KanbanCard {todo} />
+						{/if}
+					{/if}
+				{/each}
+			</Card.Root>
+			<Card.Root class="flex flex-col w-full gap-4 p-5 pt-4 min-w-fit">
+				<h2
+					class="text-xl font-semibold tracking-tight md:text-2xl first-letter:capitalize text-secondary-foreground"
 				>
-			</div>
-		{/each}
+					in-progress
+				</h2>
+				{#each $todos as todo (todo.id)}
+					{#if todo.column_number === 2}
+						<KanbanCard {todo} />
+					{/if}
+				{/each}
+			</Card.Root>
+			<Card.Root class="flex flex-col w-full gap-4 p-5 pt-4 min-w-fit">
+				<h2
+					class="text-xl font-semibold tracking-tight md:text-2xl first-letter:capitalize text-secondary-foreground"
+				>
+					done
+				</h2>
+				{#each $todos as todo (todo.id)}
+					{#if todo.column_number === 3}
+						<KanbanCard {todo} />
+					{/if}
+				{/each}
+			</Card.Root>
+		</section>
 	</section>
 </section>
